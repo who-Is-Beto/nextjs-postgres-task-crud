c9cbd00687276628c79db1526dbfd70f0e66b2d5
Roberto Cortés Monroy <robertocortesmonroy@Robertos-MacBook-Air.local>
2023-02-03 00:34:24 -0600

    [fix] baseURL

--- src/store/services/rootService.ts
+++ src/store/services/rootService.ts
@@ -3,7 +3,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const rootServices = createApi({
   reducerPath: "services",
   baseQuery: fetchBaseQuery({
-    baseUrl: document.URL || "https://nextjs-postgres-task-crud.vercel.app/"
+    baseUrl: document
+      ? document.URL
+      : "https://nextjs-postgres-task-crud.vercel.app/"
   }),
   endpoints: () => ({})
 });
