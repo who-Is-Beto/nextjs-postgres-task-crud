9a306ea07e6b5d2d8b674e94a864ae617f6038f3
Roberto Cortés <roberto.cortes@syngenta.com>
2023-03-10 20:45:44 -0600

    GHXAPI-2180[fix] dates and ux bugs

--- src/components/Modal/DeleteTask/index.tsx
+++ src/components/Modal/DeleteTask/index.tsx
@@ -1,23 +1,22 @@
-import Button from "@/components/Button/Button";
-import { Task } from "@prisma/client";
-import React from "react";
-import Styles from "./DeleteTask.module.css";
-import { useDeleteTaskMutation } from "@/store/services/TasksService";
-import { useRouter } from "next/router";
-import Loader from "@/components/Loader";
+import Button from '@/components/Button/Button';
+import { Task } from '@prisma/client';
+import React from 'react';
+import Styles from './DeleteTask.module.css';
+import { useDeleteTaskMutation } from '@/store/services/TasksService';
+import { useRouter } from 'next/router';
+import Loader from '@/components/Loader';
 
 const DeleteTask: React.FC<{
   task: Task;
   openHandler: (newIsOpen: boolean) => void;
 }> = ({ task, openHandler }) => {
-  const [deleteTask, { isLoading, isError, isSuccess }] =
-    useDeleteTaskMutation();
+  const [deleteTask, { isLoading, isError, isSuccess }] = useDeleteTaskMutation();
   const router = useRouter();
   const handleDelete = (): void => {
     deleteTask(task.id);
     if (isError) return;
     openHandler(false);
-    if (isSuccess) router.reload();
+    if (isSuccess) router.push(`/Tasks`);
   };
 
   if (isLoading) {
